6d1c3d7e7123fdff8b7edf0ffec9f35737fa2287
에케EKE <eke@bbforest.net>
2022-02-07 19:56:10 +0900

    feat: Get server time

--- routes/index.js
+++ routes/index.js
@@ -1,25 +1,44 @@
 var express = require('express');
 var router = express.Router();
 var requestIp = require('request-ip');
-const axios = require('axios');
+const https = require('https');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('index', { title: '에케 API' });
 });
 
-router.get('/ip',function(req,res){
+router.get('/ip', function (req, res) {
   let ip = requestIp.getClientIp(req);
   if (ip.startsWith('::ffff:')) ip = ip.replace('::ffff:', '');
   res.send(ip)
 });
 
-router.get('/time',function(req,res){
-  var result = axios.get(`http://bbforest.net`);
-  res.status(200).json({
-    "success" : result
-  }
-);
+router.get('/time', function (req, res){
+  res.redirect('/time/bbforest.net')
+})
+
+router.get('/time/:url', function (req, res) {
+  var url = req.params.url
+  //요청 옵션
+  https.request({
+    method: 'GET',
+    host: url,
+    path: ''
+  }, function (ress) {
+    //데이터 받을 때
+    ress.on('data', function (chunk) {
+      console.log(chunk.toString())
+    })
+    //데이터 다 받았을 때
+    ress.on('end', function () {
+      res.status(200).json({
+        "url": url,
+        "server": new Date(ress.headers.date),
+        "local": new Date()
+      });
+    })
+  }).end() //전송
 });
 
 module.exports = router;
