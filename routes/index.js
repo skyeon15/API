97c44dc175e5ef0bf73de8d5d39d314630b5c4b5
에케EKE <eke@bbforest.net>
2022-02-12 00:05:43 +0900

    fix: School lunch

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,6 @@ var express = require('express');
 var router = express.Router();
 var requestIp = require('request-ip');
 const https = require('https');
-const axios = require('axios');
-const { response } = require('../app');
 const time = require('../modules/time');
 
 /* GET home page. */
@@ -27,109 +25,113 @@ router.get('/time/:url', function (req, res) {
     host: url,
     path: ''
   }, function (ress) {
+    var send = new Date().getTime()
     //데이터 받을 때
-    ress.on('data', function (chunk){
+    ress.on('data', function (chunk) {
       //없음
     })
     //데이터 다 받았을 때
     ress.on('end', function () {
       res.status(200).json({
-        "url": url,
-        "server": time.format("YYYY-MM-DD hh:mm:ss.CCC", ress.headers.date),
-        "local": time.format("YYYY-MM-DD hh:mm:ss.CCC"),
-        "timezone": "GMT+9"
+        "url": url, 
+        "abbreviation": "KST",
+        "timezone": "Asia/Seoul",
+        "utc_offset": "UTC+09:00",
+        "url_time": time.format("YYYY-MM-DD hh:mm:ss.CCC", ress.headers.date),
+        "server_time": time.format("YYYY-MM-DD hh:mm:ss.CCC"),
+        "latency": send - new Date().getTime()
       });
     })
   }).end() //전송
 });
- 
+
 //URL 미입력시 bbforest
-router.get('/time', function (req, res){
+router.get('/time', function (req, res) {
   res.redirect('/time/bbforest.net')
 })
 
 //학교 급식 알아오기
-router.get('/school-lunch/:name', function(req, res){
+router.get('/school-lunch/:name', function (req, res) {
   //학교 정보 확인 구간
   https.request({
     method: 'GET',
     host: 'open.neis.go.kr',
     path: encodeURI('/hub/schoolInfo?Type=json&SCHUL_NM=' + req.params.name)
-  }, function(ress){
+  }, function (ress) {
     var data = ''
     //데이터 받을 때
     ress.on('data', function (chunk) {
       data += chunk
     })
     //데이터 다 받았을 때
-    ress.on('end', function(){
+    ress.on('end', function () {
       var json = JSON.parse(data), name, education, school
       if (json.schoolInfo) {
-        name = json.schoolInfo[1].row[0].SCHUL_NM //교육청코드
+        name = json.schoolInfo[1].row[0].SCHUL_NM //학교이름
         education = json.schoolInfo[1].row[0].ATPT_OFCDC_SC_CODE //교육청코드
         school = json.schoolInfo[1].row[0].SD_SCHUL_CODE  //학교코드
       }
       else res.status(200).json({
-        "INFO": [{
-          "MADE": "에케EKE(파란대나무숲BlueBambooForest)",
-          "STATUS": "오류",
-          "MESSAGE": "학교를 찾을 수 없어요. 이름을 확인해주세요.",
-          "NAME": req.params.name,
-          "DATE": time.format("YYMMDD")
-        }]
+        "STATUS": "오류-학교이름",
+        "NAME": req.params.name,
+        "DATE": time.format("YYMMDD")
       })
 
       //급식 정보 확인 구간
       https.request({
         method: 'GET',
         host: 'open.neis.go.kr',
-        path: encodeURI('/hub/mealServiceDietInfo?Type=json&ATPT_OFCDC_SC_CODE=' + education + '&SD_SCHUL_CODE=' + school + '&MLSV_YMD=210820')
-        // path: encodeURI('/hub/mealServiceDietInfo?Type=json&ATPT_OFCDC_SC_CODE=' + education + '&SD_SCHUL_CODE=' + school + '&MLSV_YMD=' + time.format("YYMMDD"))
-      }, function(ress){
+        // path: encodeURI('/hub/mealServiceDietInfo?Type=json&ATPT_OFCDC_SC_CODE=' + education + '&SD_SCHUL_CODE=' + school + '&MLSV_YMD=210820')
+        path: encodeURI('/hub/mealServiceDietInfo?Type=json&ATPT_OFCDC_SC_CODE=' + education + '&SD_SCHUL_CODE=' + school + '&MLSV_YMD=' + time.format("YYMMDD"))
+      }, function (ress) {
         var data2 = ''
-        ress.on('data', function(chuck){
+        ress.on('data', function (chuck) {
           data2 += chuck
         })
-        ress.on('end', function(){
+        ress.on('end', function () {
           json = JSON.parse(data2)
           if (json.mealServiceDietInfo) {
-            //json 만들기
-            var meat = {
-              "INFO": [{
-                "MADE": "에케EKE(파란대나무숲BlueBambooForest)",
-                "STATUS": "정상",
-                "MESSAGE": "정상 처리되었습니다.",
-                "NAME": "학교명",
-                "DATE": "날짜"
+            var meal
+            if (json.mealServiceDietInfo[1].row[2]) {
+              meal = [{
+                "meal": json.mealServiceDietInfo[1].row[0].MMEAL_SC_NM,
+                "dish": json.mealServiceDietInfo[1].row[0].DDISH_NM
+              }, {
+                "meal": json.mealServiceDietInfo[1].row[1].MMEAL_SC_NM,
+                "dish": json.mealServiceDietInfo[1].row[1].DDISH_NM
+              }, {
+                "meal": json.mealServiceDietInfo[1].row[2].MMEAL_SC_NM,
+                "dish": json.mealServiceDietInfo[1].row[2].DDISH_NM
+              }]
+            }
+            else if (json.mealServiceDietInfo[1].row[1]) {
+              meal = [{
+                "meal": json.mealServiceDietInfo[1].row[0].MMEAL_SC_NM,
+                "dish": json.mealServiceDietInfo[1].row[0].DDISH_NM
+              }, {
+                "meal": json.mealServiceDietInfo[1].row[1].MMEAL_SC_NM,
+                "dish": json.mealServiceDietInfo[1].row[1].DDISH_NM
+              }]
+            }
+            else if (json.mealServiceDietInfo[1].row[0]) {
+              meal = [{
+                "meal": json.mealServiceDietInfo[1].row[0].MMEAL_SC_NM,
+                "dish": json.mealServiceDietInfo[1].row[0].DDISH_NM
               }]
             }
 
-            //급식 있는 수 구하기
-            var row = json.mealServiceDietInfo[1].row.length.toString()
-            row *= 1
-            for(var i = 0; i < row + 1; i++){
-              meat.MEAT.i = json.mealServiceDietInfo[1].row[i].MMEAL_SC_NM
-            }
             //JSON 반환
-            res.status(200).json(meat)
-            
-            // res.status(200).json({
-            //   "school-lunch": [{
-            //     "date": json.mealServiceDietInfo[1].row[0].MLSV_YMD,  //학교명
-            //     "name": json.mealServiceDietInfo[1].row[0].SCHUL_NM,  //학교명
-            //     "meal": json.mealServiceDietInfo[1].row[0].MMEAL_SC_NM, //식사구분(조식,중식,석식)
-            //     "dish": json.mealServiceDietInfo[1].row[0].DDISH_NM //메뉴
-            //   }]
-            // })
-          }
-          else res.status(200).json({
-            "INFO": [{
-              "MADE": "에케EKE(파란대나무숲BlueBambooForest)",
-              "STATUS": "오류",
-              "MESSAGE": "급식을 찾을 수 없어요.",
+            res.status(200).json({
+              "STATUS": "정상",
               "NAME": name,
-              "DATE": time.format("YYMMDD")
-            }]
+              "DATE": time.format("YYMMDD"),
+              "meals": meal
+            })
+          }
+          res.status(200).json({
+            "STATUS": "오류-급식없음",
+            "NAME": name,
+            "DATE": time.format("YYMMDD")
           })
         })
       }).end()
