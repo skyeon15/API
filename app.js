f68ede63173ecdb445464042ec4a9b6a2d250f88
skyeon15 <eke@bbforest.net>
2022-03-01 16:48:48 +0900

    feat: logger

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var log = require('./modules/log');
+const { LOADIPHLPAPI } = require('dns');
 
 var app = express();
 
@@ -13,7 +15,14 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
-app.use(logger('dev'));
+app.all('*', function(req, res, next){
+  log.req(req)
+  if(req.hostname == 'api.bbforest.net') {
+    next()
+  }
+})
+
+// app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
