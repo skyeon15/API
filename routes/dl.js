7bb4cab34ae77af62247c9f19a5ba601c6072043
에케EKE <eke@bbforest.net>
2022-09-18 15:00:10 +0900

    fix: dl

--- routes/dl.js
+++ routes/dl.js
@@ -33,34 +33,46 @@ router.get('/*', function (req, res) {
 
     //유튜브 형식이면
     if (id != undefined) {
-        //파일 존재 여부 확인
-        if (fs.existsSync(`./data/video/yt/${id}.mp4`)) {
-            //파일이 존재하면 스트림 형식으로 호출
-            fs.createReadStream(`./data/video/yt/${id}.mp4`).pipe(res)
-        } else {
-            //파일이 존재하지 않으면 받아오기
-            var stream = ytdl(id, {
-                quality: 'highest',
-                filter: 'audioandvideo'
-            }).on('error', function (error) {
-                //오류 발생시 반환
-                res.status(404).send(error.message)
-                console.log(error.message)
-                return
-            })
+        var stream = ytdl(id, {
+            quality: 'highest',
+            filter: 'audioandvideo'
+        }).on('error', function (error) {
+            //오류 발생시 반환
+            res.status(404).send(error.message)
+            console.log(error.message)
+            return
+        })
 
-            //저장과 동시에 출력
-            var filepipe = new Stream.PassThrough()
-            var respipe = new Stream.PassThrough()
-            stream.pipe(filepipe)
-            stream.pipe(respipe)
+        stream.pipe(res)
 
-            //스트림 형식으로 저장
-            filepipe.pipe(fs.createWriteStream(`./data/video/yt/${id}.mp4`))
+        // //파일 존재 여부 확인
+        // if (fs.existsSync(`./data/video/yt/${id}.mp4`)) {
+        //     //파일이 존재하면 스트림 형식으로 호출
+        //     fs.createReadStream(`./data/video/yt/${id}.mp4`).pipe(res)
+        // } else {
+        //     //파일이 존재하지 않으면 받아오기
+        //     var stream = ytdl(id, {
+        //         quality: 'highest',
+        //         filter: 'audioandvideo'
+        //     }).on('error', function (error) {
+        //         //오류 발생시 반환
+        //         res.status(404).send(error.message)
+        //         console.log(error.message)
+        //         return
+        //     })
 
-            //출력
-            respipe.pipe(res)
-        }
+        //     //저장과 동시에 출력
+        //     var filepipe = new Stream.PassThrough()
+        //     var respipe = new Stream.PassThrough()
+        //     stream.pipe(filepipe)
+        //     stream.pipe(respipe)
+
+        //     //스트림 형식으로 저장
+        //     filepipe.pipe(fs.createWriteStream(`./data/video/yt/${id}.mp4`))
+
+        //     //출력
+        //     respipe.pipe(res)
+        // }
     }else{
         res.send({
             message: '유튜브 영상이 아닙니다.'
