8bde4f628dadcde79ad3537d47245cf025e63787
에케EKE <eke@bbforest.net>
2022-09-18 15:26:53 +0900

    fix: ytdl

--- routes/dl.js
+++ routes/dl.js
@@ -33,52 +33,46 @@ router.get('/*', function (req, res) {
 
     //유튜브 형식이면
     if (id != undefined) {
-        var stream = ytdl(id, {
-            quality: 'highest',
-            filter: 'audioandvideo'
-        }).on('error', function (error) {
-            //오류 발생시 반환
-            res.status(404).send(error.message)
-            console.log(error.message)
-            return
-        })
-
-        stream.pipe(res)
-
-        // //파일 존재 여부 확인
-        // if (fs.existsSync(`./data/video/yt/${id}.mp4`)) {
-        //     //파일이 존재하면 스트림 형식으로 호출
-        //     fs.createReadStream(`./data/video/yt/${id}.mp4`).pipe(res)
-        // } else {
-        //     //파일이 존재하지 않으면 받아오기
-        //     var stream = ytdl(id, {
-        //         quality: 'highest',
-        //         filter: 'audioandvideo'
-        //     }).on('error', function (error) {
-        //         //오류 발생시 반환
-        //         res.status(404).send(error.message)
-        //         console.log(error.message)
-        //         return
-        //     })
-
-        //     //저장과 동시에 출력
-        //     var filepipe = new Stream.PassThrough()
-        //     var respipe = new Stream.PassThrough()
-        //     stream.pipe(filepipe)
-        //     stream.pipe(respipe)
-
-        //     //스트림 형식으로 저장
-        //     filepipe.pipe(fs.createWriteStream(`./data/video/yt/${id}.mp4`))
-
-        //     //출력
-        //     respipe.pipe(res)
-        // }
-    }else{
+        //파일 존재 여부 확인
+        if (fs.existsSync(`./data/video/yt/${id}.mp4`)) {
+            //파일이 존재하면 스트림 형식으로 호출
+            fs.createReadStream(`./data/video/yt/${id}.mp4`).pipe(res)
+        } else {
+            download(id, function(){
+                fs.createReadStream(`./data/video/yt/${id}.mp4`).pipe(res)
+            })
+        }
+    } else {
         res.send({
             message: '유튜브 영상이 아닙니다.'
         })
         return
     }
 })
+
+function download(id, callback) {
+    //파일이 존재하지 않으면 받아오기
+    var stream = ytdl(id, {
+        quality: 'highest',
+        filter: 'audioandvideo'
+    }).on('error', function (error) {
+        //오류 발생시 반환
+        res.status(404).send(error.message)
+        console.log(error.message)
+        return
+    })
+
+    //저장
+    var filepipe = new Stream.PassThrough()
+    stream.pipe(filepipe)
+
+    //스트림 형식으로 저장
+    filepipe.pipe(fs.createWriteStream(`./data/video/yt/${id}.mp4`))
+
+    //스트림 다 받으면
+    stream.on('end', function(){
+        callback()
+    })
+}
 
 module.exports = router;
