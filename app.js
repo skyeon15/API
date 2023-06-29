853a777a1f259f01c04cec6498bc364edb757958
skyeon15 <hello@bbforest.net>
2023-06-29 14:16:39 +0900

    오류 핸들링

--- app.js
+++ app.js
@@ -7,13 +7,19 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var log = require('./modules/log');
-const { LOADIPHLPAPI } = require('dns');
+const fs = require('fs');
 
 var app = express();
 
-// view engine setup
+// view 엔진 설정
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
+
+// data 폴더 존재 확인
+if (!fs.existsSync('data')) {
+  // 없으면 폴더 생성
+  fs.mkdirSync('data');
+}
 
 app.all('*', function(req, res, next){
   log.req(req)
@@ -35,12 +41,12 @@ app.use('/vrc', require('./routes/VRChat.js'))
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-// catch 404 and forward to error handler
+// 404 오류 발생
 app.use(function(req, res, next) {
   next(createError(404));
 });
 
-// error handler
+// 오류 핸들링
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
